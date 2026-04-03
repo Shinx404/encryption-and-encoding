54fc344f5c6bbd507ddcfd923bb09cc78fc9d49c
Redouane Moudjaoui <moudjored@gmail.com>
2026-04-03 12:16:57 +0200

    some code added to the base64 encoding algorithm, it's all still a little confusing

--- base64/encoding.c
+++ base64/encoding.c
@@ -1,4 +1,25 @@
-int main(){
+#include <stdlib.h>
 
+
+
+
+char * encode_no_padding(char *input){
+	int size = sizeof(input)/sizeof(input[0]);
+	int *buffer = (int *)malloc(size);
+	int temp = 0x0;
+
+	for(int i = 0; i < size; i++){
+		temp = 0x0 + (int)input[i];
+		buffer[i] = temp;
+	}
+
+
+	return 
+}
+
+
+int main(int argc, char *argv[]){
+	
 	return 0;
+
 }
